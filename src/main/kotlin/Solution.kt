112d62e63662b75f171524c5166aa2f7f9208d92
Ohleesang <jammmmed@gmail.com>
2024-02-08 09:43:52 +0900

    간단한 방정식 문제

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,2 +1,10 @@
 class Solution {
+    fun solution(M: Int, N: Int) = M - 1 + (N - 1) * M
+}
+
+fun main() {
+    val a = Solution()
+    a.solution(2, 2) //3
+    a.solution(2, 5) //9
+    a.solution(1, 1) //0
 }
